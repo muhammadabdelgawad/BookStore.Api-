bf2cc8ead6434941aabebdc1eaea22b98c24d8c2
Muhammad <Muhammad@DESKTOP-OJOR7F0>
2025-07-29 19:32:03 +0300

    add dbcontext and configurations

---
+++ Data Access/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace Data_Access.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+      
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
+
---
+++ Models/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Models.Entities;
+
+namespace Models.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.CatName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            // builder.Ignore(c => c.CreatedDate);
+
+            builder.Property(c => c.CatOrder)
+                .IsRequired();
+            builder.Property<DateTime>("CreatedDate");
+        }
+    }
+}
