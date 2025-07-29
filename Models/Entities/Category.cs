476ab591f2ceffbcdfdd4bb43028964706489388
Muhammad <Muhammad@DESKTOP-OJOR7F0>
2025-07-29 19:17:27 +0300

    Add Structure for project Add Entities

---
+++ Models/Entities/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        
+        public string CatName { get; set; }
+        
+        public int CatOrder { get; set; }
+
+        public bool MarkedAsDeleted { get; set; } = false;
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Models/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        [Range(0, 10000)]
+        [Column("BookPrice")]
+        public double Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
